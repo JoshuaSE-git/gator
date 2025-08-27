5aea13b371f57d4d4324c799d360573c3fc5f579
Joshua Emralino <joshua.s.emralino@gmail.com>
2025-08-26 17:08:02 -0700

    started cli functionality

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/JoshuaSE-git/gator/internal/config"
@@ -19,4 +20,35 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", *cfg)
+}
+
+type Command struct {
+	name string
+	args []string
+}
+
+type Commands struct {
+	commandMap map[string]func(state *State, cmd Command) error
+}
+
+func (c *Commands) register(name string, handler func(state *State, cmd Command) error) {
+	c.commandMap[name] = handler
+}
+
+func handlerLogin(state *State, cmd Command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("too few arguments")
+	}
+
+	fmt.Println("Logging in...")
+
+	user := cmd.args[0]
+	err := state.Config.SetUser(user)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%v logged in successfully!\n", user)
+
+	return nil
 }
---
+++ state.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/JoshuaSE-git/gator/internal/config"
+)
+
+type State struct {
+	Config *config.Config
+}
